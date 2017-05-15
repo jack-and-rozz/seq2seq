todo: fix gradient bug

main.py
@@ -63,15 +63,21 @@ TMP_FLAGS = ['mode', 'log_file', 'checkpoint_path']
 log_file = FLAGS.log_file if FLAGS.log_file else None
 logger = common.logManager(handler=FileHandler(log_file)) if log_file else common.logManager()
 
+CHECKPOINTS_PATH = '/checkpoints'
+TESTS_PATH = '/tests'
+VARIABLES_PATH = '/variables'
+SUMMARIES_PATH = '/summaries'
 def create_dir():
   if not os.path.exists(FLAGS.checkpoint_path):
     os.makedirs(FLAGS.checkpoint_path)
-  if not os.path.exists(FLAGS.checkpoint_path + '/checkpoints'):
-    os.makedirs(FLAGS.checkpoint_path + '/checkpoints')
-  if not os.path.exists(FLAGS.checkpoint_path + '/tests'):
-    os.makedirs(FLAGS.checkpoint_path + '/tests')
-  if not os.path.exists(FLAGS.checkpoint_path + '/variables'):
-    os.makedirs(FLAGS.checkpoint_path + '/variables')
+  if not os.path.exists(FLAGS.checkpoint_path + CHECKPOINTS_PATH):
+    os.makedirs(FLAGS.checkpoint_path + CHECKPOINTS_PATH)
+  if not os.path.exists(FLAGS.checkpoint_path + TESTS_PATH):
+    os.makedirs(FLAGS.checkpoint_path + TESTS_PATH)
+  if not os.path.exists(FLAGS.checkpoint_path + VARIABLES_PATH):
+    os.makedirs(FLAGS.checkpoint_path + VARIABLES_PATH)
+  if not os.path.exists(FLAGS.checkpoint_path + SUMMARIES_PATH):
+    os.makedirs(FLAGS.checkpoint_path + SUMMARIES_PATH)
 
 def save_config():
   flags_dir = FLAGS.__dict__['__flags']
@@ -113,12 +119,22 @@ def train(sess):
   #train = ASPECDataset(FLAGS.source_data_dir, FLAGS.target_data_dir, 
   #                     FLAGS.train_data, s_vocab, t_vocab)
   logger.info("Reading dataset")
-  train = dev = test = ASPECDataset(FLAGS.source_data_dir, FLAGS.processed_data_dir, 
-                                    FLAGS.test_data, s_vocab, t_vocab)
+  train = dev = test = ASPECDataset(
+    FLAGS.source_data_dir, FLAGS.processed_data_dir, 
+    FLAGS.test_data, s_vocab, t_vocab)
   dataset = common.dotDict({'train': train, 'dev': dev, 'test' : test})
-  
-  train_model = create_model(sess)
-  test_model = create_model(sess, reuse=True)
+  with tf.name_scope('train'):
+    mtrain = create_model(sess)
+  summary_writer = tf.summary.FileWriter(
+    FLAGS.checkpoint_path + SUMMARIES_PATH, sess.graph) 
+  exit(1)
+  for i, raw_batch in enumerate(test.get_batch(FLAGS.batch_size)):
+    batch = test.padding_and_format(raw_batch, BUCKETS)
+    mtrain.step(sess, batch)
+    exit(1)
+  exit(1)
+  with tf.name_scope('dev'):
+    mtest = create_model(sess, reuse=True)
   pass
 
 def main(_):

models.py
@@ -10,52 +10,55 @@ import tensorflow as tf
 #import tensorflow.contrib.rnn as rnn
 from tensorflow.python.ops import variable_scope
 from tensorflow.contrib.rnn.python.ops import core_rnn_cell as rnn_cell
+from tensorflow.python.ops import init_ops
 
 #from tensorflow.contrib.rnn.python.ops import core_rnn_cell as rnn_cell
 #import tensorflow.contrib.legacy_seq2seq as seq2seq
 from seq2seq import RNNEncoder, BidirectionalRNNEncoder, RNNDecoder, BasicSeq2Seq
 
 from utils import common
-from utils import dataset as data_utils
+from utils.dataset import PAD_ID, GO_ID, EOS_ID, UNK_ID
 dtype=tf.float32
 
 class Baseline(object):
   def __init__(self, FLAGS, buckets, forward_only=False):
     self.buckets = buckets
     self.read_flags(FLAGS)
-    self.setup_placeholders()
+    with tf.name_scope('placeholders'):
+      self.setup_placeholders(use_sequence_length=True)
     cell = self.setup_cell(forward_only)
-    #encoder = BidirectionalRNNEncoder(
     with variable_scope.variable_scope("Encoder") as encoder_scope:
       encoder_embedding = self.initialize_embedding(FLAGS.source_vocab_size,
                                                     FLAGS.embedding_size)
-      encoder = RNNEncoder(cell, encoder_embedding,
-                           scope=encoder_scope)
+      encoder = BidirectionalRNNEncoder(cell, encoder_embedding,
+                           scope=encoder_scope, 
+                           sequence_length=self.sequence_length)
 
     with variable_scope.variable_scope("Decoder") as decoder_scope:
       decoder_embedding = self.initialize_embedding(FLAGS.target_vocab_size,
                                                     FLAGS.embedding_size)
       decoder = RNNDecoder(copy.deepcopy(cell), decoder_embedding, 
                            scope=decoder_scope)
-    self.seq2seq = BasicSeq2Seq(encoder, decoder, FLAGS.num_samples)
-    self.outputs, self.losses = self.seq2seq(
-      self.encoder_inputs, self.decoder_inputs,
-      self.targets, self.target_weights, self.buckets)
-    if not forward_only:
-      self.updates = self.setup_updates(self.losses)
+    with variable_scope.variable_scope('Seq2Seq') as seq2seq_scope:
+      self.seq2seq = BasicSeq2Seq(encoder, decoder, FLAGS.num_samples,
+                                  feed_previous=forward_only)
+      self.outputs, self.losses = self.seq2seq(
+        self.encoder_inputs, self.decoder_inputs,
+        self.targets, self.target_weights, self.buckets)
+      if not forward_only:
+        self.updates = self.setup_updates(self.losses)
 
-  def initialize_embedding(self, vocab_size, embedding_size, scope=None):
-    with variable_scope.variable_scope(scope):
-      sqrt3 = math.sqrt(3)  # Uniform(-sqrt(3), sqrt(3)) has variance=1.
-      initializer = init_ops.random_uniform_initializer(-sqrt3, sqrt3)
-      embedding = variable_scope.get_variable(
-        "embedding", [vocab_size, embedding_size],
-        initializer=initializer)
-      return embedding
+  def initialize_embedding(self, vocab_size, embedding_size):
+    sqrt3 = math.sqrt(3)  # Uniform(-sqrt(3), sqrt(3)) has variance=1.
+    initializer = init_ops.random_uniform_initializer(-sqrt3, sqrt3)
+    embedding = variable_scope.get_variable(
+      "embedding", [vocab_size, embedding_size],
+      initializer=initializer)
+    return embedding
 
   def setup_updates(self, losses):
     params = tf.trainable_variables()
-    #self.gradient_norms = []
+    self.gradient_norms = []
     updates = []
     opt = tf.train.AdamOptimizer(self.learning_rate)
 
@@ -63,18 +66,15 @@ class Baseline(object):
       gradients = tf.gradients(loss, params)
       clipped_gradients, norm = tf.clip_by_global_norm(gradients,
                                                        self.max_gradient_norm)
-      #print clipped_gradients
-      #print params
-      for c,p in zip(clipped_gradients, params):
+      for c,p in zip(gradients, params):
         print c, p
-      exit(1)
-      #self.gradient_norms.append(norm)
+      self.gradient_norms.append(norm)
       updates.append(opt.apply_gradients(
-        zip(clipped_gradients, params)[4], global_step=self.global_step))
+        zip(clipped_gradients, params), global_step=self.global_step))
       exit(1)
     return updates
 
-  def setup_placeholders(self):
+  def setup_placeholders(self, use_sequence_length=True):
     # Feeds for inputs.
     self.encoder_inputs = []
     self.decoder_inputs = []
@@ -91,6 +91,8 @@ class Baseline(object):
     # Our targets are decoder inputs shifted by one.
     self.targets = [self.decoder_inputs[i + 1]
                     for i in xrange(len(self.decoder_inputs) - 1)]
+    self.sequence_length = tf.placeholder(tf.int32, shape=[None], name="sequence_length") if use_sequence_length else None
+
 
   def setup_cell(self, forward_only):
     cell = getattr(rnn_cell, self.cell_type)(self.hidden_size, reuse=tf.get_variable_scope().reuse) 
@@ -120,178 +122,36 @@ class Baseline(object):
       "global_step", trainable=False, shape=[1],
       initializer=tf.constant_initializer(0, dtype=np.int32)) 
 
-  def get_batch(self, data):
-    pass
-  # def setup_seq2seq(self, forward_only):
-  #   buckets = self.buckets
-  #   softmax_loss_function = self.softmax_loss_function
-  #   output_projection = self.output_projection
-  #   def seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
-  #     return getattr(seq2seq, self.seq2seq_type)(
-  #       encoder_inputs,
-  #       decoder_inputs,
-  #       self.cell,
-  #       self.cell2,
-  #       num_encoder_symbols=self.source_vocab_size,
-  #       num_decoder_symbols=self.target_vocab_size,
-  #       embedding_size=self.embedding_size,
-  #       output_projection=self.output_projection,
-  #       feed_previous=do_decode,
-  #       dtype=dtype,
-  #       scope='Seq2Seq'
-  #     )
-  #   # Feeds for inputs.
-  #   self.encoder_inputs = []
-  #   self.decoder_inputs = []
-  #   self.target_weights = []
-  #   for i in xrange(buckets[-1][0]):  # Last bucket is the biggest one.
-  #     self.encoder_inputs.append(tf.placeholder(tf.int32, shape=[None],
-  #                                               name="encoder{0}".format(i)))
-  #   for i in xrange(buckets[-1][1] + 1):
-  #     self.decoder_inputs.append(tf.placeholder(tf.int32, shape=[None],
-  #                                               name="decoder{0}".format(i)))
-  #     self.target_weights.append(tf.placeholder(dtype, shape=[None],
-  #                                               name="weight{0}".format(i)))
+  def get_input_feed(self, batch):
+    input_feed = {}
+    batch_size = batch.encoder_inputs
+    encoder_size, decoder_size = self.buckets[batch.bucket_id]
+    for l in xrange(encoder_size):
+      input_feed[self.encoder_inputs[l].name] = batch.encoder_inputs[l]
+    for l in xrange(decoder_size):
+      input_feed[self.decoder_inputs[l].name] = batch.decoder_inputs[l]
+      input_feed[self.target_weights[l].name] = batch.target_weights[l]
+    if self.sequence_length != None: 
+      input_feed[self.sequence_length.name] = batch.target_weights[l]
 
-  #   # Our targets are decoder inputs shifted by one.
-  #   targets = [self.decoder_inputs[i + 1]
-  #              for i in xrange(len(self.decoder_inputs) - 1)]
+    # Since our targets are decoder inputs shifted by one, we need one more.
+    last_target = self.decoder_inputs[decoder_size].name
+    input_feed[last_target] = np.zeros([batch.size], dtype=np.int32)
+    return input_feed 
 
-  #   # Training outputs and losses.
-  #   if forward_only:
-  #     self.outputs, self.losses = seq2seq.model_with_buckets(
-  #         self.encoder_inputs, self.decoder_inputs, targets,
-  #         self.target_weights, buckets, lambda x, y: seq2seq_f(x, y, True),
-  #         softmax_loss_function=softmax_loss_function)
-  #     # If we use output projection, we need to project outputs for decoding.
-  #     if output_projection is not None:
-  #       for b in xrange(len(buckets)):
-  #         self.outputs[b] = [
-  #             tf.matmul(output, output_projection[0]) + output_projection[1]
-  #             for output in self.outputs[b]
-  #         ]
-  #   else:
-  #     self.outputs, self.losses = seq2seq.model_with_buckets(
-  #         self.encoder_inputs, self.decoder_inputs, targets,
-  #         self.target_weights, buckets,
-  #         lambda x, y: seq2seq_f(x, y, False),
-  #         softmax_loss_function=self.softmax_loss_function)
+  def step(self, sess, batch):
+    bucket_id = batch.bucket_id
+    input_feed = self.get_input_feed(batch)
+    exit(1)
+    # Output feed: depends on whether we do a backward step or not.
+    if not forward_only:
+      output_feed = [self.updates[bucket_id],  # Update Op that does SGD.
+                     self.gradient_norms[bucket_id],  # Gradient norm.
+                     self.losses[bucket_id]]  # Loss for this batch.
+    else:
+      output_feed = [self.losses[bucket_id]]  # Loss for this batch.
+      for l in xrange(decoder_size):  # Output logits.
+        output_feed.append(self.outputs[bucket_id][l])
+    outputs = sess.run(output_feed, input_feed)
 
-  #   # Gradients and SGD update operation for training the model.
-  #   params = tf.trainable_variables()
-  #   if not forward_only:
-  #     self.gradient_norms = []
-  #     self.updates = []
-  #     opt = tf.train.AdamOptimizer(self.learning_rate)
-  #     for b in xrange(len(buckets)):
-  #       gradients = tf.gradients(self.losses[b], params)
-  #       clipped_gradients, norm = tf.clip_by_global_norm(gradients,
-  #                                                        self.max_gradient_norm)
-  #       self.gradient_norms.append(norm)
-  #       self.updates.append(opt.apply_gradients(
-  #           zip(clipped_gradients, params), global_step=self.global_step))
 
-  # def step(self, session, encoder_inputs, decoder_inputs, target_weights,
-  #          bucket_id, forward_only):
-  #   """Run a step of the model feeding the given inputs.
-
-  #   Args:
-  #     session: tensorflow session to use.
-  #     encoder_inputs: list of numpy int vectors to feed as encoder inputs.
-  #     decoder_inputs: list of numpy int vectors to feed as decoder inputs.
-  #     target_weights: list of numpy float vectors to feed as target weights.
-  #     bucket_id: which bucket of the model to use.
-  #     forward_only: whether to do the backward step or only forward.
-
-  #   Returns:
-  #     A triple consisting of gradient norm (or None if we did not do backward),
-  #     average perplexity, and the outputs.
-
-  #   Raises:
-  #     ValueError: if length of encoder_inputs, decoder_inputs, or
-  #       target_weights disagrees with bucket size for the specified bucket_id.
-  #   """
-  #   # Check if the sizes match.
-  #   encoder_size, decoder_size = self.buckets[bucket_id]
-  #   if len(encoder_inputs) != encoder_size:
-  #     raise ValueError("Encoder length must be equal to the one in bucket,"
-  #                      " %d != %d." % (len(encoder_inputs), encoder_size))
-  #   if len(decoder_inputs) != decoder_size:
-  #     raise ValueError("Decoder length must be equal to the one in bucket,"
-  #                      " %d != %d." % (len(decoder_inputs), decoder_size))
-  #   if len(target_weights) != decoder_size:
-  #     raise ValueError("Weights length must be equal to the one in bucket,"
-  #                      " %d != %d." % (len(target_weights), decoder_size))
-
-  #   # Input feed: encoder inputs, decoder inputs, target_weights, as provided.
-  #   input_feed = {}
-  #   for l in xrange(encoder_size):
-  #     input_feed[self.encoder_inputs[l].name] = encoder_inputs[l]
-  #   for l in xrange(decoder_size):
-  #     input_feed[self.decoder_inputs[l].name] = decoder_inputs[l]
-  #     input_feed[self.target_weights[l].name] = target_weights[l]
-
-  #   # Since our targets are decoder inputs shifted by one, we need one more.
-  #   last_target = self.decoder_inputs[decoder_size].name
-  #   input_feed[last_target] = np.zeros([self.batch_size], dtype=np.int32)
-
-  #   # Output feed: depends on whether we do a backward step or not.
-  #   if not forward_only:
-  #     output_feed = [self.updates[bucket_id],  # Update Op that does SGD.
-  #                    self.gradient_norms[bucket_id],  # Gradient norm.
-  #                    self.losses[bucket_id]]  # Loss for this batch.
-  #   else:
-  #     output_feed = [self.losses[bucket_id]]  # Loss for this batch.
-  #     for l in xrange(decoder_size):  # Output logits.
-  #       output_feed.append(self.outputs[bucket_id][l])
-
-  #   outputs = session.run(output_feed, input_feed)
-  #   if not forward_only:
-  #     return outputs[1], outputs[2], None  # Gradient norm, loss, no outputs.
-  #   else:
-  #     return None, outputs[0], outputs[1:]  # No gradient norm, loss, outputs.
-
-  # def get_batch(self, data, bucket_id):
-  #   encoder_size, decoder_size = self.buckets[bucket_id]
-  #   encoder_inputs, decoder_inputs = [], []
-
-  #   # Get a random batch of encoder and decoder inputs from data,
-  #   # pad them if needed, reverse encoder inputs and add GO to decoder.
-  #   for _ in xrange(self.batch_size):
-  #     encoder_input, decoder_input = random.choice(data[bucket_id])
-
-  #     # Encoder inputs are padded and then reversed.
-  #     encoder_pad = [data_utils.PAD_ID] * (encoder_size - len(encoder_input))
-  #     encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
-
-  #     # Decoder inputs get an extra "GO" symbol, and are padded then.
-  #     decoder_pad_size = decoder_size - len(decoder_input) - 1
-  #     decoder_inputs.append([data_utils.GO_ID] + decoder_input +
-  #                           [data_utils.PAD_ID] * decoder_pad_size)
-
-  #   # Now we create batch-major vectors from the data selected above.
-  #   batch_encoder_inputs, batch_decoder_inputs, batch_weights = [], [], []
-
-  #   # Batch encoder inputs are just re-indexed encoder_inputs.
-  #   for length_idx in xrange(encoder_size):
-  #     batch_encoder_inputs.append(
-  #         np.array([encoder_inputs[batch_idx][length_idx]
-  #                   for batch_idx in xrange(self.batch_size)], dtype=np.int32))
-
-  #   # Batch decoder inputs are re-indexed decoder_inputs, we create weights.
-  #   for length_idx in xrange(decoder_size):
-  #     batch_decoder_inputs.append(
-  #         np.array([decoder_inputs[batch_idx][length_idx]
-  #                   for batch_idx in xrange(self.batch_size)], dtype=np.int32))
-
-  #     # Create target_weights to be 0 for targets that are padding.
-  #     batch_weight = np.ones(self.batch_size, dtype=np.float32)
-  #     for batch_idx in xrange(self.batch_size):
-  #       # We set weight to 0 if the corresponding target is a PAD symbol.
-  #       # The corresponding target is decoder_input shifted by 1 forward.
-  #       if length_idx < decoder_size - 1:
-  #         target = decoder_inputs[batch_idx][length_idx + 1]
-  #       if length_idx == decoder_size - 1 or target == data_utils.PAD_ID:
-  #         batch_weight[batch_idx] = 0.0
-  #     batch_weights.append(batch_weight)
-  #   return batch_encoder_inputs, batch_decoder_inputs, batch_weights

seq2seq.py
@@ -24,7 +24,7 @@ from tensorflow.python.ops import variable_scope
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import init_ops
-from tensorflow.contrib.legacy_seq2seq import sequence_loss, sequence_loss_by_example, rnn_decoder, model_with_buckets
+from tensorflow.contrib.legacy_seq2seq import sequence_loss, sequence_loss_by_example, rnn_decoder #, model_with_buckets
 # TODO(ebrevdo): Remove once _linear is fully deprecated.
 #linear = core_rnn_cell_impl._linear  # pylint: disable=protected-access
 
@@ -104,6 +104,7 @@ def projection_and_sampled_loss(target_vocab_size, hidden_size, num_samples):
       labels = tf.reshape(labels, [-1, 1])
       # We need to compute the sampled_softmax_loss using 32bit floats to
       # avoid numerical instabilities.
+      dtype=tf.float32
       local_w_t = tf.cast(w_t, tf.float32)
       local_b = tf.cast(b, tf.float32)
       local_inputs = tf.cast(logits, tf.float32)
@@ -129,13 +130,13 @@ class Decoder(object):
 
 
 class RNNEncoder(Encoder):
-  def __init__(self, cell, embedding 
-               scope=None, sequence_length=None, activation=math_ops.tanh):
+  def __init__(self, cell, embedding, sequence_length=None,
+               scope=None, activation=math_ops.tanh):
     with variable_scope.variable_scope(scope or "rnn_encoder") as scope:
       self.cell = cell
       self.embedding = embedding
-      self.sequence_length = sequence_length
       self.activation = activation
+      self.sequence_length = sequence_length
   @property
   def state_size(self):
     return self.cell.state_size
@@ -158,8 +159,7 @@ class RNNDecoder(Decoder):
   def __init__(self, cell, embedding, scope=None):
     with variable_scope.variable_scope(scope or "rnn_decoder") as scope:
       self.cell = cell
-      self.embedding = initialize_embedding(vocab_size, embedding_size) if not embedding else embedding
-
+      self.embedding=embedding
   @property
   def state_size(self):
     return self.cell.state_size
@@ -187,14 +187,14 @@ class RNNDecoder(Decoder):
     return outputs, state
 
 class BidirectionalRNNEncoder(RNNEncoder):
-  def __init__(self, cell, vocab_size, embedding_size, 
-               scope=None, sequence_length=None, activation=math_ops.tanh):
+  def __init__(self, cell, embedding, sequence_length=None,
+               scope=None, activation=math_ops.tanh):
     with variable_scope.variable_scope(scope or "bidirectional_rnn_encoder"):
       self.cell = self.cell_fw = cell
       self.cell_bw = copy.deepcopy(cell)
-      self.embedding = initialize_embedding(vocab_size, embedding_size)
-      self.sequence_length = sequence_length
+      self.embedding = embedding
       self.activation = activation
+      self.sequence_length=sequence_length
 
   def __call__(self, inputs, scope=None, dtype=np.float32):
     with variable_scope.variable_scope(scope or "bidirectional_rnn_encoder"):
@@ -204,7 +204,6 @@ class BidirectionalRNNEncoder(RNNEncoder):
         self.cell_fw, self.cell_bw, embedded,
         sequence_length=self.sequence_length,
         scope=scope, dtype=dtype)
-      state = array_ops.concat([state_fw, state_bw], 1)
       hsize = self.state_size
       w = tf.get_variable("proj_w", [hsize * 2, hsize])
       b = tf.get_variable("proj_b", [hsize])
@@ -218,25 +217,26 @@ class BasicSeq2Seq(object):
     self.decoder = decoder
     self.projection, self.loss = projection_and_sampled_loss(
       decoder.embedding.shape[0], decoder.cell.output_size, num_samples)
-    self.loop_function=_extract_argmax_and_embed(
-      self.decoder.embedding, self.projection) if feed_previous else None
+    self.loop_function = None
+    if feed_previous:
+      self.loop_function = _extract_argmax_and_embed(
+        self.decoder.embedding, output_projection=self.projection)
 
   def seq2seq(self, encoder_inputs, decoder_inputs):
     with variable_scope.variable_scope("Encoder") as scope:
-      encoder_outputs, encoder_state = self.encoder(encoder_inputs, scope=scope)
+      encoder_outputs, encoder_state = self.encoder(
+        encoder_inputs, scope=scope)
     with variable_scope.variable_scope("Decoder") as scope:
       decoder_outputs, decoder_state = self.decoder(
         decoder_inputs, encoder_state, scope=scope,
         loop_function=self.loop_function)
     return decoder_outputs, decoder_state
 
-  def __call__(self, encoder_inputs,
-               decoder_inputs,
-               targets,
-               weights,
-               buckets):
+  def __call__(self, encoder_inputs, decoder_inputs, targets, weights,
+               buckets, per_example_loss=False):
     outputs, losses = model_with_buckets(
       encoder_inputs, decoder_inputs, targets, weights, buckets,
-      self.seq2seq, per_example_loss=True)
+      self.seq2seq, per_example_loss=per_example_loss, 
+      softmax_loss_function=self.loss)
     return outputs, losses
 

utils/dataset.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import division
 #from __future__ import print_function
 
-import gzip, os, re, tarfile, json, sys, collections, types, commands
+import gzip, os, re, tarfile, json, sys, collections, types, commands, itertools
 import MeCab
 import numpy as np
 import mojimoji
@@ -114,24 +114,7 @@ class ASPECDataset(object):
     t_data = self.initialize_data(source_dir, target_dir, filename, t_vocab)
     self.data = sorted([(i, s, t) for i,(s,t) in enumerate(zip(s_data, t_data))], 
                        key=lambda x: len(x[1]))
-    #self.data = [(i, s, t) for i,(s,t) in enumerate(zip(s_data, t_data))]
-    print "Corpus Statistics"
-    lens = [len(d[1]) for d in self.data]
-    lent = [len(d[2]) for d in self.data]
-    print 'Source: (min, max, ave) = (%d, %d, %.2f)' % (min(lens), max(lens), sum(lens)/len(lens))
-    print 'Target: (min, max, ave) = (%d, %d, %.2f)' % (min(lent), max(lent), sum(lent)/len(lent))
-    
-    print "Corpus Example"
-    for i, s, t in self.data[:3]:
-      print '<%d>' %i
-      print " ".join(self.s_vocab.to_tokens(s))
-      print " ".join(self.t_vocab.to_tokens(t))
-     
 
-  def get_batch(self, batch_size):
-    pass
-
-  #@common.timewatch()
   def initialize_data(self, source_dir, target_dir, filename, vocab):
     source_path = os.path.join(source_dir, filename) + '.%s' % vocab.lang
     processed_path = os.path.join(target_dir, filename) + '.%s.Wids%d' % (vocab.lang, vocab.size)
@@ -145,3 +128,83 @@ class ASPECDataset(object):
       with open(processed_path, 'w') as f:
         f.write('\n'.join([' '.join([str(x) for x in l]) for l in data]))
     return data
+
+  def stat(self):
+    print "Corpus Statistics"
+    lens = [len(d[1]) for d in self.data]
+    lent = [len(d[2]) for d in self.data]
+    print 'Source: (min, max, ave) = (%d, %d, %.2f)' % (min(lens), max(lens), sum(lens)/len(lens))
+    print 'Target: (min, max, ave) = (%d, %d, %.2f)' % (min(lent), max(lent), sum(lent)/len(lent))
+
+
+  def get_batch(self, batch_size):
+    for i, d in itertools.groupby(enumerate(self.data), 
+                                     lambda x: x[0] // batch_size):
+      raw_batch = tuple(x[1] for x in d)
+      yield raw_batch
+
+
+  def padding_and_format(self, data, buckets, do_reverse=False, 
+                         use_sequence_length=True):
+    '''
+    Caution:  if both do_reverse and use_sequence_length are True at the same time, many PAD_IDs and only a small part of a sentence are read.
+    '''
+    batch_size = len(data)
+    max_s = max([len(x[1]) for x in data])
+    max_t = max([len(x[2]) for x in data])
+    bucket_id = min([i for i, (s, t) in enumerate(buckets) 
+                   if max_s <= s and max_t <= s-2])
+    encoder_size, decoder_size = buckets[bucket_id]
+    encoder_inputs, decoder_inputs, encoder_sequence_length = [], [], []
+    for _, encoder_input, decoder_input in data:
+      encoder_sequence_length.append(len(encoder_input))
+      # Encoder inputs are padded and then reversed if do_reverse=True.
+      encoder_pad = [PAD_ID] * (encoder_size - len(encoder_input))
+      encoder_input = encoder_input + encoder_pad
+      if do_reverse:
+        encoder_input = list(reversed(encoder_input))
+      encoder_inputs.append(encoder_input)
+
+      # Decoder inputs get an extra "GO" and "EOSsymbol, and are padded then.
+      decoder_pad_size = decoder_size - len(decoder_input) - 2
+      decoder_inputs.append([GO_ID] + decoder_input + [EOS_ID] +
+                            [PAD_ID] * decoder_pad_size)
+
+    # Now we create batch-major vectors from the data selected above.
+    batch_encoder_inputs, batch_decoder_inputs, batch_weights = [], [], []
+
+    # Batch encoder inputs are just re-indexed encoder_inputs.
+    for length_idx in xrange(encoder_size):
+      batch_encoder_inputs.append(
+          np.array([encoder_inputs[batch_idx][length_idx]
+                    for batch_idx in xrange(batch_size)], dtype=np.int32))
+
+    # Batch decoder inputs are re-indexed decoder_inputs, we create weights.
+    for length_idx in xrange(decoder_size):
+      batch_decoder_inputs.append(
+          np.array([decoder_inputs[batch_idx][length_idx]
+                    for batch_idx in xrange(batch_size)], dtype=np.int32))
+
+      # Create target_weights to be 0 for targets that are padding.
+      batch_weight = np.ones(batch_size, dtype=np.float32)
+      for batch_idx in xrange(batch_size):
+        # We set weight to 0 if the corresponding target is a PAD symbol.
+        # The corresponding target is decoder_input shifted by 1 forward.
+        if length_idx < decoder_size - 1:
+          target = decoder_inputs[batch_idx][length_idx + 1]
+        if length_idx == decoder_size - 1 or target == PAD_ID:
+          batch_weight[batch_idx] = 0.0
+      batch_weights.append(batch_weight)
+    if not use_sequence_length:
+      encoder_sequence_length = None 
+    batch = common.dotDict({
+      'encoder_inputs' : batch_encoder_inputs,
+      'decoder_inputs' : batch_decoder_inputs,
+      'target_weights' : batch_weights,
+      'sequence_length' : encoder_sequence_length,
+      'bucket_id': bucket_id,
+      'size' : batch_size,
+    })
+    return batch
+    #return batch_encoder_inputs, batch_decoder_inputs, batch_weights, encoder_sequence_length
+