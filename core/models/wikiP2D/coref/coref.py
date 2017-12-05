Fix the bug in embedding normalization (not to normalize each embedding, instead of the embedding matrix)

core/models/wikiP2D/coref/coref.py
@@ -1,6 +1,6 @@
 # coding: utf-8 
 import operator
-import math, time, sys, random
+import math, time, sys, random, re
 import tensorflow as tf
 from core.utils import common, tf_utils
 from core.models.base import ModelBase
@@ -270,6 +270,25 @@ class CoreferenceResolution(ModelBase):
     if self.use_metadata:
       input_feed[self.speaker_ids] = np.array(speaker_ids)
       input_feed[self.genre] = np.array(genre)
+
+    #########Debug
+    # with open('feed_dict.txt', 'w') as f:
+    #   sys.stdout = f
+    #   for k, v in input_feed.items():
+    #     if re.search('w_sentences', k.name):
+    #       print k.name
+    #       print self.sess.run(tf.nn.embedding_lookup(self.encoder.w_embeddings, self.w_sentences), input_feed)
+    #     else:
+    #       print k.name
+    #       print v
+    #   w = 'this'
+    #   w_id = self.encoder.w_vocab.token2id(w)
+    #   print w, w_id
+    #   print self.sess.run(tf.nn.embedding_lookup(self.encoder.w_embeddings, tf.constant(w_id)))
+    #   sys.stdout = sys.__stdout__
+    # exit(1)
+    #########
+
     if is_training and len(w_sentences) > self.max_training_sentences:
       return self.truncate_example(input_feed)
     else:
@@ -277,9 +296,6 @@ class CoreferenceResolution(ModelBase):
 
   #def truncate_example(self, word_emb, char_index, text_len, speaker_ids, genre, is_training, gold_starts, gold_ends, cluster_ids):
   def truncate_example(self, input_feed):
-    #print input_feed[self.w_sentences].shape
-    #print input_feed[self.c_sentences].shape
-    #exit(1)
     max_training_sentences = self.max_training_sentences
     num_sentences = input_feed[self.w_sentences].shape[0]
     assert num_sentences > max_training_sentences

core/models/wikiP2D/encoder.py
@@ -1,6 +1,6 @@
 # coding: utf-8 
+import sys
 import tensorflow as tf
-
 from core.utils import common, tf_utils
 from core.utils.tf_utils import shape, cnn, linear
 from core.seq2seq.rnn import setup_cell
@@ -35,18 +35,32 @@ class WordEncoder(ModelBase):
 
     if self.wbase:
       w_pretrained = True if isinstance(w_vocab, VocabularyWithEmbedding) else False
-      w_initializer = tf.constant_initializer(w_vocab.embeddings) if w_pretrained else None
-      w_emb_shape = w_vocab.embeddings.shape if w_pretrained else [w_vocab.size, config.w_embedding_size] 
       w_trainable = config.trainable_emb or not w_pretrained
+      if w_pretrained:
+        sys.stderr.write("Initialize word embeddings with the pretrained.\n")
+        w_initializer = tf.constant_initializer(w_vocab.embeddings)
+        w_emb_shape = w_vocab.embeddings.shape
+      else:
+        w_initializer = None
+        w_emb_shape = [w_vocab.size, config.w_embedding_size] 
+      print 'w_trainable', w_trainable
+      
       self.w_embeddings = self.initialize_embeddings('word_emb', w_emb_shape, initializer=w_initializer, trainable=w_trainable)
 
     if self.cbase:
       c_pretrained = True if isinstance(c_vocab, VocabularyWithEmbedding) else False
-      c_initializer = tf.constant_initializer(c_vocab.embeddings) if c_pretrained else None
-      c_emb_shape = w_vocab.embeddings.shape if c_pretrained else [c_vocab.size, config.c_embedding_size] 
-
       c_trainable = config.trainable_emb or not c_pretrained
+
+      if c_pretrained:
+        sys.stderr.write("Initialize character embeddings with the pretrained.\n")
+        c_initializer = tf.constant_initializer(c_vocab.embeddings)
+        c_emb_shape = c_vocab.embeddings.shape 
+      else: 
+        c_initializer = None
+        c_emb_shape = [c_vocab.size, config.c_embedding_size] 
+
       self.c_embeddings = self.initialize_embeddings('char_emb', c_emb_shape, initializer=c_initializer, trainable=c_trainable)
+      print 'c_trainable', c_trainable
 
   def encode(self, wc_inputs):
     # inputs: the list of [None, max_sentence_length] or [None, max_sentence_length, max_word_length]

core/vocabulary/base.py
@@ -4,7 +4,7 @@ from tensorflow.python.platform import gfile
 from orderedset import OrderedSet
 import core.utils.common as common
 import numpy as np
-from sklearn.preprocessing import normalize
+#from sklearn.preprocessing import normalize
 
 _PAD = "_PAD"
 _BOS = "_BOS"
@@ -235,10 +235,12 @@ class VocabularyWithEmbedding(WordVocabularyBase):
     vocab = collections.OrderedDict({t:i for i,t in enumerate(rev_vocab)})
 
     # Merge pretrained embeddings and allocate zero vectors to START_VOCAB.
-    embeddings = [common.flatten([emb[w] for emb in pretrained]) for w in vocab]
-    embeddings = np.array(embeddings)
     if self.normalize_embedding:
-      embeddings = normalize(embeddings, axis=1)
+      # Normalize the pretrained embeddings for each of the embedding types.
+      embeddings = [common.flatten([common.normalize_vector(emb[w]) for emb in pretrained]) for w in vocab]
+    else:
+      embeddings = [common.flatten([emb[w] for emb in pretrained]) for w in vocab]
+    embeddings = np.array(embeddings)
     return vocab, rev_vocab, embeddings
 
   def load(self, embedding_path, embedding_format):